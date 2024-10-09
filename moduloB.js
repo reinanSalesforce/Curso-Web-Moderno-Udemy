d42b6eb73137a30d7fb82beac593a9ef724f717d
Reinan Oliveira <66265820+reinanSalesforce@users.noreply.github.com>
2024-10-09 02:18:44 -0300

    Node

---
+++ moduloB.js
@@ -0,0 +1,8 @@
+// Forma mais comum de acessar um modulo
+
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
---
+++ moduloCliente.js
@@ -0,0 +1,14 @@
+// Importando modulos para ser usados nesse moduloCliente
+
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+console.log(moduloA);
+
+
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
+console.log(moduloB);
