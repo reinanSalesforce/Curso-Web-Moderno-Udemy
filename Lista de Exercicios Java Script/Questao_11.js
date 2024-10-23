097e9b27838a36fb822af7804118bf7a37b8dab9
Projeto Agil <projetoagil.businessagil.com>
2024-10-23 01:03:26 -0300

    Questao 11

--- Lista de Exercicios Java Script/Questao_11.js
+++ Lista de Exercicios Java Script/Questao_11.js
@@ -1,3 +1,9 @@
 const receberPrimeiroEUltimoElemento = (numArray = []) => {
-    
+    let primeiroElemento = numArray.shift();
+    let ultimoElemento = numArray.pop();
+
+    let resultadoArray = [primeiroElemento,ultimoElemento];
+    console.log(resultadoArray);
 }
+
+receberPrimeiroEUltimoElemento([7,2,4,5,1,34]);
